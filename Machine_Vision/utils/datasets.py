4ebbceedb6fff6364194ed70a3d0c70dcd1c2a7a
E-Eckmann <ethan.jnerd.eckmann@gmail.com>
2025-08-27 16:26:57 -0500

    Delete Machine_Vision directory

--- Machine_Vision/utils/datasets.py
+++
@@ -1,8 +0,0 @@
-def get_labels(dataset_name):
-    if dataset_name == 'fer2013':
-        return {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy',
-                4: 'sad', 5: 'surprise', 6: 'neutral'}
-    elif dataset_name == 'imdb':
-        return {0: 'woman', 1: 'man'}
-    else:
-        raise Exception('Invalid dataset name')
